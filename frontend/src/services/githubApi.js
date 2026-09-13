134c5b21fad54018e48ae74bac62d38b791ae710
SROR7 <m7mdsror0@gmail.com>
2026-09-13 06:12:29 +0300

    finish the k8s section

--- frontend/src/services/githubApi.js
+++ frontend/src/services/githubApi.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/github",
+  baseURL: "/api/github",
 });
-
 export const getGithubUser = async (username) => {
   const response = await api.get(`/${username}`);
-  return response.data;
+  return response.data; 
 };
 
 export const getGithubRepos = async (username) => {
