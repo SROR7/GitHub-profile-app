8192c2c87fb43bc01b4c5c08571cd02be5bb0e86
SROR7 <m7mdsror0@gmail.com>
2026-09-16 01:12:16 +0300

    edit the server section in vite.config

--- Terraform/vpc.tf
+++ Terraform/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "gitddev-vpc"
+  name = "gitops-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'k8s-gitdevap-githubpr-3695d52823-543529870.eu-north-1.elb.amazonaws.com'
+    ],
+  },
 })
